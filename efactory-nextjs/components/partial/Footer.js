4bd4245a1fc961449ef8485002110bf60e4e2b78
Walter Perchinumio <wperchinumio@gmail.com>
2025-09-05 19:10:43 -0700

    WIP

--- efactory-nextjs/components/partial/Footer.js
+++ efactory-nextjs/components/partial/Footer.js
@@ -6,15 +6,5 @@ export default function Footer({ className }) {
 
   const CurrentYear = new Date().getFullYear()
 
-  return (
-    <div className={`${className ? className : ''} footer md:p-6 sm:p-3 py-3 mt-auto`}>
-      <div className='container-fluid flex items-center justify-between gap-15 md:flex-row flex-col md:text-[16px]/[24px] text-[14px]/[20px]'>
-        <p className='text-font-color-100 text-center'>
-          © {CurrentYear} <span className='text-primary'>eFactory</span>, All Rights Reserved.
-        </p>
-        <div />
-        <div />
-      </div>
-    </div>
-  )
+  return null
 }
