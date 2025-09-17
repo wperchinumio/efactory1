7884399f4344fc092c45aae10d43f3515a80f598
Walter Perchinumio <wperchinumio@gmail.com>
2025-09-17 07:47:24 -0700

    WIP

--- efactory-nextjs/src/pages/overview/item-overview.tsx
+++ efactory-nextjs/src/pages/overview/item-overview.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@/components/layout/Layout';
 import ItemOverview from '@/components/overview/ItemOverview';
