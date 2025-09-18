b23aea3bcb22858b13727183fa20394278456c97
Walter Perchinumio <wperchinumio@gmail.com>
2025-09-17 21:33:39 -0700

    WIP: favicon

--- efactory-nextjs/src/pages/_app.tsx
+++ efactory-nextjs/src/pages/_app.tsx
@@ -58,7 +58,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/logo_square.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
         <title>eFactory</title>
       </Head>
       <AuthErrorBoundary>
